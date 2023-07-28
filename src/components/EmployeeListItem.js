2c8a892e4e012819423ae465f6fc0467d5f293d9
rimi <a.fatimaarimi@gmail.com>
2023-07-28 00:14:19 -0400

    restructerd the employee list items

--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -1,14 +1,15 @@
 import "../App.css";
 
-function EmployeeListItem() {
-  const employees = [
-    {name: "James King", role: "President and CEO"}, 
-    {name: "Julie Taylor", role: "VP of Marketing"}, 
-    {name: "Eugene Lee", role: "CFO"}, 
-    {name: "John Williams", role: "VP of Engineering"}, 
-    {name: "Ray Moore", role: "VP of Sales"}, 
-    {name: "Paul Jones", role: "QA Manager"}
+const employees = [
+  {name: "James King", role: "President and CEO"}, 
+  {name: "Julie Taylor", role: "VP of Marketing"}, 
+  {name: "Eugene Lee", role: "CFO"}, 
+  {name: "John Williams", role: "VP of Engineering"}, 
+  {name: "Ray Moore", role: "VP of Sales"}, 
+  {name: "Paul Jones", role: "QA Manager"}
 ];
+
+function EmployeeListItem() {
   return (
     <div className="employee-list-item">
       {employees.map((employee, index) => (
