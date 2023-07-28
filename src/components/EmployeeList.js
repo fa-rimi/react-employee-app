aaea199ca91b1f3d77336d3e6a41e9ad3ff5fd4a
rimi <a.fatimaarimi@gmail.com>
2023-07-28 00:16:18 -0400

    fixed files

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,15 +1,29 @@
 import "../App.css";
 import EmployeeListItem from "../components/EmployeeListItem";
 
-function EmployeeList() {
-    return (
-    
-        <div className="employee-list">
-            <EmployeeListItem/>
-            {console.log("EmployeeList is live!")}
-        </div>
+const employees = [
+  { name: "James King", role: "President and CEO" },
+  { name: "Julie Taylor", role: "VP of Marketing" },
+  { name: "Eugene Lee", role: "CFO" },
+  { name: "John Williams", role: "VP of Engineering" },
+  { name: "Ray Moore", role: "VP of Sales" },
+  { name: "Paul Jones", role: "QA Manager" },
+];
 
-    )
+function EmployeeList() {
+  return (
+    <div className="employee-list-item">
+      {employees.map((employee, index) => (
+        <EmployeeListItem
+          key={index}
+          name={employee.name}
+          role={employee.role}
+        />
+      ))}
+      {console.log("EmployeeList generating...")}
+      {console.log(employees)}
+    </div>
+  );
 }
 
 export default EmployeeList;
--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -1,26 +1,9 @@
 import "../App.css";
 
-const employees = [
-  {name: "James King", role: "President and CEO"}, 
-  {name: "Julie Taylor", role: "VP of Marketing"}, 
-  {name: "Eugene Lee", role: "CFO"}, 
-  {name: "John Williams", role: "VP of Engineering"}, 
-  {name: "Ray Moore", role: "VP of Sales"}, 
-  {name: "Paul Jones", role: "QA Manager"}
-];
-
 function EmployeeListItem() {
   return (
-    <div className="employee-list-item">
-      {employees.map((employee, index) => (
-        <EmployeeListItem
-          key={index}
-          name={employee.name}
-          role={employee.role}
-        />
-      ))}
-      {/* {console.log("EmployeeListItem generating...")} */}
-      {console.log(employees)}
+    <div>
+      {console.log("EmployeeListItem generating...")}
     </div>
   );
 }
