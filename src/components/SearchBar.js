9ea9eb8de161db934efa5392446b67cce8f248c0
rimi <a.fatimaarimi@gmail.com>
2023-07-27 23:13:57 -0400

    search bar outline

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import "../App.css";
+
+function SearchBar() {
+    return (
+
+        <div className="search-bar">
+            <input>
+            </input>
+            {console.log("SearchBar is Active!")}
+        </div>
+
+    )
+}
+
+export default SearchBar;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import "../App.css";
 import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
 
 function HomePage() {
   return (
 
     <div className="home-pg">
       <Header/>
+      <SearchBar/>
       {console.log("Homepage is connected")}
     </div>
 
