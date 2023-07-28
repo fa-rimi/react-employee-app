6653f600e324f16fd9e0b5001e227c3fa7347031
rimi <a.fatimaarimi@gmail.com>
2023-07-27 23:56:00 -0400

    added to EmployeeListItem with the employees

--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -1,13 +1,27 @@
 import "../App.css";
 
 function EmployeeListItem() {
-    return (
-
-        <div className="employee-list-item">
-            {console.log("EmployeeListItem generating...")}
-        </div>
-
-    )
+  const employees = [
+    {name: "James King", role: "President and CEO"}, 
+    {name: "Julie Taylor", role: "VP of Marketing"}, 
+    {name: "Eugene Lee", role: "CFO"}, 
+    {name: "John Williams", role: "VP of Engineering"}, 
+    {name: "Ray Moore", role: "VP of Sales"}, 
+    {name: "Paul Jones", role: "QA Manager"}
+];
+  return (
+    <div className="employee-list-item">
+      {employees.map((employee, index) => (
+        <EmployeeListItem
+          key={index}
+          name={employee.name}
+          role={employee.role}
+        />
+      ))}
+      {/* {console.log("EmployeeListItem generating...")} */}
+      {console.log(employees)}
+    </div>
+  );
 }
 
 export default EmployeeListItem;
